269835daed8d3839c56736db8c0dc65d20ce37c6
NicoleSharrock <n.sharrockhayes@gmail.com>
2022-03-12 08:17:31 -0500

    added functions

--- Develop/script.js
+++ Develop/script.js
@@ -7,16 +7,7 @@ const getRandomUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "
 const getRandomLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 const getRandomNumber = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 const getRandomSymbol = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "{", "}", "[", "]", ",", "'", "/", "<", ">",];
-const maxLength = [128]
-
-const randomFun = {
-  upper: getRandomUpper,
-  lower: getRandomLower,
-  number: getRandomNumber,
-  Symbol: getRandomSymbol
-}
-
-
+const maxLength = 128
 
 
 function generatePassword() {
@@ -27,21 +18,34 @@ function generatePassword() {
   if (maxLength > 7 && maxLength < 129) {
 
 
-    var getRandomsymbol = window.confirm("Click OK to confirm including special characters");
+    var symbol = window.confirm("Click OK to confirm including special characters");
 
-    var getRandomNumber = window.confirm("Click OK to confirm including numeric characters");
+    var number = window.confirm("Click OK to confirm including numeric characters");
 
-    var getRandomLower = window.confirm("Click OK to confirm including uppercase characters");
+    var lower = window.confirm("Click OK to confirm including uppercase characters");
 
-    var getRandomUpper = window.confirm("Click OK to confirm including lowercase characters");
+    var upper = window.confirm("Click OK to confirm including lowercase characters");
   } else {
     window.alert("Invaild entry. Please input a length between 8 and 128");
-    return generatePassword;
+    return !generatePassword;
   }
 
   var pool = [];
 
-  if (getRandomSymbol == true) pool.push(..)
+  if (symbol == true) pool.push(...getRandomSymbol);
+  if (number == true) pool.push(...getRandomNumber);
+  if (lower == true) pool.push(...getRandomLower);
+  if (upper == true) pool.push(...getRandomUpper)
+
+  if (getRandomSymbol || getRandomNumber || getRandomLower || getRandomUpper) {
+    for (var i = 0; i < maxLength; i++) {
+      results += pool[Math.floor(Math.random() * pool.length)];
+    }
+  } else {
+    window.alert("A minimum of 1 variable must be selected to generate a paaword.")
+    return generatePassword();
+  }
+  return results;
 }
 
 // Write password to the #password input
