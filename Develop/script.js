7910a5005cda7753bd9a8f42c53b41bcf6b217c5
NicoleSharrock <n.sharrockhayes@gmail.com>
2022-03-12 07:48:36 -0500

    add click boxes

--- Develop/script.js
+++ Develop/script.js
@@ -3,13 +3,46 @@ var generateBtn = document.querySelector("#generate");
 
 
 // Assignment code here
-var getRandomUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var getRandomLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var getRandomNumber = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var getRandomSymbol = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "{", "}", "[", "]", ",", "'", "/", "<", ">",];
+const getRandomUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const getRandomLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const getRandomNumber = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const getRandomSymbol = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "{", "}", "[", "]", ",", "'", "/", "<", ">",];
+const maxLength = [128]
+
+const randomFun = {
+  upper: getRandomUpper,
+  lower: getRandomLower,
+  number: getRandomNumber,
+  Symbol: getRandomSymbol
+}
 
 
 
+
+function generatePassword() {
+  var results = "";
+  var numberOfCharacters = window.prompt("How many characters would you like your password to contain?");
+  var maxLength = parseInt(numberOfCharacters);
+
+  if (maxLength > 7 && maxLength < 129) {
+
+
+    var getRandomsymbol = window.confirm("Click OK to confirm including special characters");
+
+    var getRandomNumber = window.confirm("Click OK to confirm including numeric characters");
+
+    var getRandomLower = window.confirm("Click OK to confirm including uppercase characters");
+
+    var getRandomUpper = window.confirm("Click OK to confirm including lowercase characters");
+  } else {
+    window.alert("Invaild entry. Please input a length between 8 and 128");
+    return generatePassword;
+  }
+
+  var pool = [];
+
+  if (getRandomSymbol == true) pool.push(..)
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -19,20 +52,6 @@ function writePassword() {
   passwordText.value = password;
 
 }
-var getRandomLength = prompt("How many characters would you like your password to contain?")
-
-var getRandomsymbol = window.confirm("Click OK to confirm including special characters")
-
-var getRandomNumber = window.confirm("Click OK to confirm including numeric characters")
-
-var getRandomLower = window.confirm("Click OK to confirm including uppercase characters")
-
-var getRandomUpper = window.confirm("Click OK to confirm including lowercase characters")
-
-// function generatePassword() {
-//   return prompt("this is a prompt")
-// }
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
